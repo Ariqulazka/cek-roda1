63d9d044bd5b52dd1bd4e84183a4b5c3f2ca010a
Muhammad Ariqulazka <56347648+Ariqulazka@users.noreply.github.com>
2025-05-31 15:11:25 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,8 +4,6 @@ plugins {
     id("kotlin-kapt")
     alias(libs.plugins.google.gms.google.services)
     alias(libs.plugins.google.firebase.crashlytics)
-    alias(libs.plugins.google.gms.google.services)
-    alias(libs.plugins.google.firebase.crashlytics)
 }
 
 android {
